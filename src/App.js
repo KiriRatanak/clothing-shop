df6403ba365140ce38e086770e290903b43933fb
Kiriratanak Vorn <sir.ratanak@gmail.com>
2020-09-23 01:04:51 +0700

    add header with logo and options. set global padding in App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 
@@ -15,6 +16,7 @@ import './App.css';
 function App() {
   return (
     <div>
+			<Header />
       <Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
