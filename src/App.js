9358d70202f3b6bc4aca7ca79fb4981df6d9f5d0
Kiriratanak Vorn <sir.ratanak@gmail.com>
2020-09-23 00:22:51 +0700

    implement shoop page with collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+
 import HatsPage from './pages/hats-page/hatspage.component'
 import JacketsPage from './pages/jackets-page/jackets-page.component'
 import SneakersPage from './pages/sneakers-page/sneakers-page.componnet'
@@ -15,6 +17,7 @@ function App() {
     <div>
       <Switch>
 				<Route exact path="/" component={HomePage} />
+				<Route exact path="/shop" component={ShopPage} />
 				<Route exact path="/shop/hats" component={HatsPage} />
 				<Route exact path="/shop/jackets" component={JacketsPage} />
 				<Route exact path="/shop/sneakers" component={SneakersPage} />
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import SHOP_DATA from './shop.data'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+class ShopPage extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			collections: SHOP_DATA
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Collections</h1>
+				{
+					this.state.collections.map(({id, ...otherCollectionProps}) => (
+						<CollectionPreview key={id} {...otherCollectionProps} />
+					))
+				}
+				
+			</div>
+		)
+	}
+}
+
+export default ShopPage
